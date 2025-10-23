a868a83e4340695f26feb5477bd1da45d9ac536c
HozFisen <hozfisen@gmail.com>
2025-10-24 02:00:00 +0700

    register validation

--- routers/posts.js
+++ routers/posts.js
@@ -3,9 +3,10 @@ const postController = require('../controllers/postController');
 const {auth} = require('../middleware/authentication')
 
 // router.get("/", postController.posts); 
-router.get("/:id/like", auth ,postController.like); 
+
 router.get("/add", auth, postController.getPost); 
-router.post("/add", auth, postController.postPost); 
+router.post("/add", auth, postController.postPost);
+router.get("/:id/like", auth ,postController.like); 
 router.post("/:id/delete", auth, postController.delete); 
 
 module.exports = router;
